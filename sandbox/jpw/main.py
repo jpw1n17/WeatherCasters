9cbcdb3b38a7cf974d45fa30fe5736adfcecb8c4
Philip Wilson <jpw1n17@soton.ac.uk>
2018-02-14 16:20:05 +0000

    now skips model creation if it can load it instead, saves 5 mins on each run

--- sandbox/jpw/main.py
+++ sandbox/jpw/main.py
@@ -8,6 +8,10 @@ from nltk.corpus import stopwords
 
 import gensim
 
+#global variables
+g_data_path = '../../data/'
+g_output_path = './gen_data/'
+
 def load_csv(file_path):
     data_obj = {}
     headers = []
@@ -46,68 +50,56 @@ def create_dir_if_not_exist(dir_path):
     if not exists(dir_path):
         makedirs(dir_path)
 
-# start of main script
-data_path = '../../data/'
-output_path = './gen_data/'
-tweet_id_header_col_index = 0
-tweet_header_col_index = 1
-create_dir_if_not_exist(output_path)
+def load_or_create_vector_space(docs, ids):
+    if len(docs) != len(ids):
+        raise Exception('docs and ids must be of same length')
+    
+    d2v_model = {}
+    vec_space_model_path = g_output_path + 'doc2vec.model'
+    if exists(vec_space_model_path):
+        print('Loading doc2vec model')
+        return gensim.models.Doc2Vec.load(vec_space_model_path)
+    else:
+        # create from scratch
+        print('Creating doc2vec model')
+        tagged_docs = link_doc_to_id(docs, ids)
+        d2v_model = gensim.models.Doc2Vec(min_count=0, alpha=0.025, min_alpha=0.025)
+        d2v_model.build_vocab(tagged_docs)
+        print('    created vocab')
+        d2v_model.train(tagged_docs, total_examples=len(tagged_docs), epochs=100)
+        print('    trained model')
+        d2v_model.save(vec_space_model_path)
+        return d2v_model
 
+def main():
+    tweet_id_header_col_index = 0
+    tweet_header_col_index = 1
+    create_dir_if_not_exist(g_output_path)
 
-source_data = load_csv(data_path + 'train.csv')
-headers = source_data['headers']
-tweet_header_label = headers[tweet_header_col_index]
-tweet_id_header_label = headers[tweet_header_col_index]
+    print('Loading source data')
+    source_data = load_csv(g_data_path + 'train.csv')
+    headers = source_data['headers']
+    tweet_header_label = headers[tweet_header_col_index]
+    tweet_id_header_label = headers[tweet_header_col_index]
+    tweet_ids = source_data[tweet_id_header_label]
+    raw_tweets = source_data[tweet_header_label]
 
-tweet_ids = source_data[tweet_id_header_label]
-raw_tweets = source_data[tweet_header_label]
-clean_tweets = nlp_clean(raw_tweets)
-print('data loaded and cleaned')
-linked_tweets = link_doc_to_id(clean_tweets, tweet_ids)
-print('data linked')
-model = gensim.models.Doc2Vec(min_count=0, alpha=0.025, min_alpha=0.025)
-model.build_vocab(linked_tweets)
-print('vocab built')
+    print('cleaning tweets')
+    clean_tweets = nlp_clean(raw_tweets)
+    
+    d2vm = load_or_create_vector_space(clean_tweets, tweet_ids)
 
-# get the initial document vector, and most similar articles
-# (before training, the results should be wrong)
-docvec1 = list(model.docvecs[0])
-docvecsyn1 = model.docvecs.doctag_syn0[0]
-docsim1 = model.docvecs.most_similar(tweet_ids[0])
+    print('vectors space trained, most similar too "strom" are...')
+    for item in d2vm.wv.most_similar(positive=['storm'], topn=4):
+        print(item)
 
-#training of model
-model.train(linked_tweets, total_examples=len(linked_tweets), epochs=100)
-print('vectors space trained')
+    print('\nvectors space trained, most similar too "storm" while suppresing "wind" are...')
+    for item in d2vm.wv.most_similar(positive=['storm'], negative=['wind'], topn=4):
+        print(item)
 
-# get the trained document vector, and most similar articles
-# (after training, the results should be correct)
-docvec2 = model.docvecs[0]
-docvecsyn2 = model.docvecs.doctag_syn0[0]
-docsim2 = model.docvecs.most_similar(tweet_ids[0])
+    print('\nvectors space trained, most similar too "storm" while suppresing "rain" are...')
+    for item in d2vm.wv.most_similar(positive=['storm'], negative=['rain'], topn=4):
+        print(item)
 
-# print results
-# document vector
-print('Document vector:')
-
-# before training
-print('(Before training)')
-print('number of dimensions' + str(len(docvec1)))
-print(docvec1[:5])
-print(docvecsyn1[:5])
-
-print('(After training)')
-print(docvec2[:5])
-print(docvecsyn2[:5])
-
-# most similar documents
-print('\nMost similar:')
-
-# before training, the result is wrong. after training, correct. good.
-print('(Before training)')
-print(docsim1[:2])
-
-print('(After training)')
-print(docsim2[:2])
-
-model.save(output_path + 'doc2vec.model')
-print('model saved')
+# start of main script 
+main()
