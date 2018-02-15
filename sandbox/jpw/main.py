d4aa4d25497468018435f14120debda3b2b4059c
Philip Wilson <jpw1n17@soton.ac.uk>
2018-02-15 16:09:56 +0000

    tidyed up some code, dose the same as before

--- sandbox/jpw/main.py
+++ sandbox/jpw/main.py
@@ -6,34 +6,41 @@ from nltk import RegexpTokenizer
 from nltk.corpus import stopwords
 import gensim
 
+# for simplisity I'm inserting custom data into pandas dataframe
+# this is fine as long as I don't use any operations which copy 
+# dataframes.  I'm supressing the warning for now.
+import warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
+
 #global variables
 g_data_path = '../../data/'
 g_output_path = './gen_data/'
 
-def nlp_clean(data):
+def add_cleaned_tweets(data):
+    print('cleaning tweets')
     tokenizer = RegexpTokenizer(r'\w+')
     stopword_set = set(stopwords.words('english'))
-    new_data = []
-    for d in data:
+    data.cleaned_tweets = []
+    for d in data.tweet:
         new_str = d.lower()
         ordered_list = tokenizer.tokenize(new_str)
         clipped_list = list(set(ordered_list).difference(stopword_set)) # side effect of changing word order
         clipped_ordered_list = [word for word in ordered_list if word in clipped_list]
-        new_data.append(clipped_ordered_list)
-    return new_data
+        data.cleaned_tweets.append(clipped_ordered_list)
 
-def link_doc_to_id(docs, ids):
-    linked_docs = []
-    if len(docs) != len(ids):
-        raise Exception('must have same number of docs as ids')
-    for doc, id in zip(docs, ids):
-        linked_docs.append(gensim.models.doc2vec.TaggedDocument(doc, [id]))
-    return linked_docs
-
-def load_or_create_vector_space(docs, ids):
-    if len(docs) != len(ids):
-        raise Exception('docs and ids must be of same length')
-    
+def add_link_doc_to_id(data):
+    ids = data.id
+    docs = data.cleaned_tweets
+    raw_docs = data.tweet
+    data.lookup = {}
+    data.lookup_raw = {}
+    data.tagged_docs = []
+    for doc, raw, id in zip(docs, raw_docs, ids):
+        data.lookup[id]=doc
+        data.lookup_raw[id]=raw
+        data.tagged_docs.append(gensim.models.doc2vec.TaggedDocument(doc, [id]))
+   
+def load_or_create_vector_space(data):
     d2v_model = {}
     vec_space_model_path = g_output_path + 'doc2vec.model'
     if exists(vec_space_model_path):
@@ -42,44 +49,47 @@ def load_or_create_vector_space(docs, ids):
     else:
         # create from scratch
         print('Creating doc2vec model')
-        tagged_docs = link_doc_to_id(docs, ids)
         d2v_model = gensim.models.Doc2Vec(min_count=0, alpha=0.025, min_alpha=0.025)
         d2v_model.build_vocab(tagged_docs)
         print('    created vocab')
-        d2v_model.train(tagged_docs, total_examples=len(tagged_docs), epochs=100)
+        d2v_model.train(data.tagged_docs, total_examples=len(data.tagged_docs), epochs=100)
         print('    trained model')
         d2v_model.save(vec_space_model_path)
         return d2v_model
 
-def main():
-    makedirs(g_output_path, exist_ok=True)
-
-    print('Loading source data')
-    #source_data = load_csv(g_data_path + 'train.csv')
-    source_data = pd.read_csv(g_data_path + 'train.csv')
-
-    print('cleaning tweets')
-    clean_tweets = nlp_clean(source_data['tweet'])
-
-    d2vm = load_or_create_vector_space(clean_tweets, source_data['id'])
-
+def d2v_inspection(model, data):
     print('vectors space trained, most similar too "strom" are...')
-    for item in d2vm.wv.most_similar(positive=['storm'], topn=4):
+    for item in model.wv.most_similar(positive=['storm'], topn=4):
         print(item)
 
     print('\nvectors space trained, most similar too "storm" while suppresing "wind" are...')
-    for item in d2vm.wv.most_similar(positive=['storm'], negative=['wind'], topn=4):
+    for item in model.wv.most_similar(positive=['storm'], negative=['wind'], topn=4):
         print(item)
 
     print('\nvectors space trained, most similar too "storm" while suppresing "rain" are...')
-    for item in d2vm.wv.most_similar(positive=['storm'], negative=['rain'], topn=4):
+    for item in model.wv.most_similar(positive=['storm'], negative=['rain'], topn=4):
         print(item)
 
     test_str = 'looks like another sunny day tomorrow'
     print('\n\ntweets similar to "' +test_str+ '" are ...')
-    test_vec = d2vm.infer_vector(test_str.split())
-    for tweet in d2vm.docvecs.most_similar([test_vec], topn=3):
-        print(tweet)
+    test_vec = model.infer_vector(test_str.split())
+    for item in model.docvecs.most_similar([test_vec], topn=3):
+        tweet_id = item[0]
+        likelihood = item[1]
+        tweet_text = data.lookup_raw[tweet_id]
+        print(str(likelihood) + '\t' + tweet_text)
+
+def main():
+    makedirs(g_output_path, exist_ok=True)
+
+    print(gensim.__version__)
+
+    print('Loading source data')
+    data = pd.read_csv(g_data_path + 'train.csv')
+    add_cleaned_tweets(data)
+    add_link_doc_to_id(data)
+    d2vm = load_or_create_vector_space(data)
+    d2v_inspection(d2vm, data)
 
 # start of main script 
 main()
