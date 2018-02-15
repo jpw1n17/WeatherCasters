2decd7637a55e796daad85326aa61e9489a13cda
Philip Wilson <jpw1n17@soton.ac.uk>
2018-02-15 16:40:45 +0000

    I've added a custom class to remove the need to supress warnings. it makes the code a bit longer however its better than not know if things are going wrong

--- sandbox/jpw/main.py
+++ sandbox/jpw/main.py
@@ -1,44 +1,48 @@
 from os import makedirs
 from os.path import exists
 import pandas as pd
+import copy
 
 from nltk import RegexpTokenizer
 from nltk.corpus import stopwords
 import gensim
 
-# for simplisity I'm inserting custom data into pandas dataframe
-# this is fine as long as I don't use any operations which copy 
-# dataframes.  I'm supressing the warning for now.
-import warnings
-warnings.simplefilter(action='ignore', category=UserWarning)
-
 #global variables
 g_data_path = '../../data/'
 g_output_path = './gen_data/'
+
+class CustomDataFrame(pd.DataFrame):
+    bag = {}
+    def __init__(self, *args, **kw):
+        super(CustomDataFrame, self).__init__(*args, **kw)
+ 
+    @property
+    def _constructor(self):
+        return CustomDataFrame
 
 def add_cleaned_tweets(data):
     print('cleaning tweets')
     tokenizer = RegexpTokenizer(r'\w+')
     stopword_set = set(stopwords.words('english'))
-    data.cleaned_tweets = []
+    data.bag['cleaned_tweets'] = []
     for d in data.tweet:
         new_str = d.lower()
         ordered_list = tokenizer.tokenize(new_str)
         clipped_list = list(set(ordered_list).difference(stopword_set)) # side effect of changing word order
         clipped_ordered_list = [word for word in ordered_list if word in clipped_list]
-        data.cleaned_tweets.append(clipped_ordered_list)
+        data.bag['cleaned_tweets'].append(clipped_ordered_list)
 
 def add_link_doc_to_id(data):
     ids = data.id
-    docs = data.cleaned_tweets
+    docs = data.bag['cleaned_tweets']
     raw_docs = data.tweet
-    data.lookup = {}
-    data.lookup_raw = {}
-    data.tagged_docs = []
+    data.bag['lookup'] = {}
+    data.bag['lookup_raw'] = {}
+    data.bag['tagged_docs'] = []
     for doc, raw, id in zip(docs, raw_docs, ids):
-        data.lookup[id]=doc
-        data.lookup_raw[id]=raw
-        data.tagged_docs.append(gensim.models.doc2vec.TaggedDocument(doc, [id]))
+        data.bag['lookup'][id]=doc
+        data.bag['lookup_raw'][id]=raw
+        data.bag['tagged_docs'].append(gensim.models.doc2vec.TaggedDocument(doc, [id]))
    
 def load_or_create_vector_space(data):
     d2v_model = {}
@@ -52,7 +56,7 @@ def load_or_create_vector_space(data):
         d2v_model = gensim.models.Doc2Vec(min_count=0, alpha=0.025, min_alpha=0.025)
         d2v_model.build_vocab(tagged_docs)
         print('    created vocab')
-        d2v_model.train(data.tagged_docs, total_examples=len(data.tagged_docs), epochs=100)
+        d2v_model.train(data.bag['tagged_docs'], total_examples=len(data.bag['tagged_docs']), epochs=100)
         print('    trained model')
         d2v_model.save(vec_space_model_path)
         return d2v_model
@@ -76,16 +80,16 @@ def d2v_inspection(model, data):
     for item in model.docvecs.most_similar([test_vec], topn=3):
         tweet_id = item[0]
         likelihood = item[1]
-        tweet_text = data.lookup_raw[tweet_id]
+        tweet_text = data.bag['lookup_raw'][tweet_id]
         print(str(likelihood) + '\t' + tweet_text)
 
 def main():
     makedirs(g_output_path, exist_ok=True)
 
-    print(gensim.__version__)
+    # print(gensim.__version__)
 
     print('Loading source data')
-    data = pd.read_csv(g_data_path + 'train.csv')
+    data = CustomDataFrame(pd.read_csv(g_data_path + 'train.csv'))
     add_cleaned_tweets(data)
     add_link_doc_to_id(data)
     d2vm = load_or_create_vector_space(data)
