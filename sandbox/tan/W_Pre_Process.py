29aef4ef51321f858f330e06eaa56fdfe41681fa
namfonrt <32952193+namfonrt@users.noreply.github.com>
2018-02-18 07:33:53 +1100

    [Preprocessing] - Category "W" done
    - Remove train.csv file
    - All done

--- sandbox/tan/W_Pre_Process.py
+++ sandbox/tan/W_Pre_Process.py
@@ -1,3 +1,14 @@
+##Note Include only these words
+#- tuple[1] start with "V"/
+#- modal verb "MD"/
+#- custom "adv of time", "point of time",/
+#- custom "day, month, year, season, time" day name/ month name/ season name
+# No Lemmatizer
+#remark
+# - after "to" if the word no meaning and lowercase will be verb
+ 
+
+
 #Some used imp
 import json
 import nltk
@@ -9,59 +20,49 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import PorterStemmer
 import pandas as pd
 import re
-import string   # string.punctuation '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~’ 
+import string   
 import emot
 #from emoticon import EmojiWordReader
 
  # Load a CSV	
-train_data = pd.read_csv('train.csv'
+train_data = pd.read_csv('../../data/train.csv'
                          ,names = ["id", "tweets", "state", "location", "s1", "s2", "s3", "s4", "s5", "w1", "w2", "w3", "w4", "k1", "k2", "k3", "k4", "k5", "k6", "k7", "k8", "k9", "k10", "k11", "k12", "k13", "k14", "k15"]
                          ,header=0,sep=',',error_bad_lines=False,encoding='utf-8')
 
-#Assign variables
-
-#Don'y care Emoticon
-
+#Don't care Emoticon
 baseRegx="\w+|\!|\?"
 regx= baseRegx
 tokenized_text = []
 tokenizer = RegexpTokenizer(regx)
 
-#Get english stopwords
-eng_stopwords = stopwords.words('english') 
-
-#Define Lemmatizer
-lemmatizer = WordNetLemmatizer()
+#Define adverb of time and noun of time
+#https://www.englishclub.com/vocabulary/adverbs-time.htm
+time_adv= ["now","then","today","tomorrow","tonight","yesterday","annually","daily","fortnightly","hourly","monthly","nightly","quarterly","weekly","yearly","always","constantly","ever","frequently","generally","infrequently","never","normally","occasionally","often","rarely","regularly","seldom","sometimes","regularly","usually","already","before","early","earlier","eventually","finally","first","formerly","just","last","late","later","lately","next","previously","recently","since","soon","still","yet"]
+# http://www.english-for-students.com/Noun-Words-for-Time.html
+time_noun = ["afternoon","age","beginning","calendar","century","clock","date","dawn","day","decade","end","era","evening","forenoon","fortnight","future","hour","midday","midnight","minute","month","morning","night","noon","past","present","previous","season","second","sunrise","sunset","today","tomorrow","week","year","yesterday"]   
+time_month= ["january","jan","february","feb","march","mar","april","apr","may","june","jun","july","jul","august","aug","september","sep","sept","october","oct","november","nov","december","dec"]
+time_day = ["sunday","sun","monday","mon","tuesday","tue","tues","wednesday","wed","thursday","thu","thur","thurs","friday","fri","saturday","sat"]
+time_season = ["spring","summer","autumn","fall","winter"]
+time_custom = ["forecast","day","month","year","season"]
+time_word_list = time_adv + time_noun + time_month + time_day + time_season + time_custom
 
 #Start pre-processing
 for tweet in train_data.tweets:
-    #Lowercase
-    lower_case = tweet.lower()
-    
-    #####Note
-    #- tuple[1] start with "V"
-    #- modal verb "MD"
-    #- custom "adv of time", "point of time",
-    #words = ["last","yet","tomorrow","yesterday", "next", "is", "am", "were","go","gone","going", "can", "might", "can't", "couldn't" ]
-    #pos = nltk.pos_tag(words)
-    ######
-    
     #Tokenize
-    tokens = tokenizer.tokenize(lower_case)
-    
-    #Re-initial token list in each round
-    filtered_tokens=[] 
-    
-    #Remove stop word
-    # ?should we remove stopword? see row 20, 21 as "can't" can tell something about sentiment
-    for word in tokens:
-        if not word in eng_stopwords:
-            #Lemmatize 
-            lemmatized = lemmatizer.lemmatize(word, pos="v")
-            filtered_tokens.append(lemmatized)
+    tokens = tokenizer.tokenize(tweet)   
+               
+    #Pos tagging
+    append_pos = []
+    tagged_tokens = nltk.pos_tag(tokens)
+    for posTag in tagged_tokens: 
+        # Tagging is case sensitive, so lowwer needs to be after
+        lower_word = posTag[0].lower()
         
+        #Keep all verbs, Modal verb, words of time 
+        if (posTag[1].startswith("V") 
+            or posTag[1] == "MD" 
+            or lower_word in time_word_list) :
+            append_pos.append(lower_word)  
+            
     #Append each tokenized tweet in the list
-    tokenized_text.append(filtered_tokens)
-    #tokenized_text
-    
-#Tagging
+    tokenized_text.append(append_pos)
