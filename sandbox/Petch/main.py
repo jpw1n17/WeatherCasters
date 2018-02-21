fb9bf7a15413b49f9bdaafb304c42b909371b9a0
Petch <32884740+howtolovexz@users.noreply.github.com>
2018-02-21 13:53:34 +0000

    Update main.py
    - Add 3d version

--- sandbox/Petch/main.py
+++ sandbox/Petch/main.py
@@ -8,6 +8,7 @@ Created on Wed Feb 14 16:54:52 2018
 import gensim
 import csv
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from nltk import RegexpTokenizer
 from nltk.corpus import stopwords
@@ -57,13 +58,26 @@ def create_model(it, size):
     #start testing
     #printing the vector of document at index 1 in docLabels
     docvec = d2v_model.docvecs[1]
-    for i in range(0, 9):
+    for i in range(0, 19):
         docvec = d2v_model.docvecs[i]
-        plt.quiver([0], [0], [docvec.item(0)], [docvec.item(1)], angles='xy', scale_units='xy', scale=1)
-        plt.xlim(-0.3, 0.3)
-        plt.ylim(-0.3, 0.3)
-        plt.grid(True)
-        plt.show
+        print(train_data[i])
+        print(docvec)
+        
+        # 2d version
+        #plt.quiver([0], [0], [docvec.item(0)], [docvec.item(1)], angles='xy', scale_units='xy', scale=1)
+        #plt.xlim(-0.3, 0.3)
+        #plt.ylim(-0.3, 0.3)
+        #plt.grid(True)
+        #plt.show
+        
+        # 3d version
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.quiver([0], [0], [0], [docvec.item(0)], [docvec.item(1)], [docvec.item(2)])
+        ax.set_xlim([-0.2, 0.2])
+        ax.set_ylim([-0.2, 0.2])
+        ax.set_zlim([-0.2, 0.2])
+        plt.show()
         
     
     #printing the vector of the file using its name
@@ -105,4 +119,4 @@ data = nlp_clean(data)
 #iterator returned over all documents
 it = LabeledLineSentence(data, docLabels)
 #create_model(it, 10)
-create_model(it, 2)
+create_model(it, 3)
