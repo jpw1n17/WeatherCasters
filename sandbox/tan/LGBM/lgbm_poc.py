38df0a0eab9a7fd89cd2980c0daac8b60f4dc1b5
kinetiz <thanadon.f@gmail.com>
2018-04-28 00:41:24 +0100

    plot all result

--- sandbox/tan/LGBM/lgbm_poc.py
+++ sandbox/tan/LGBM/lgbm_poc.py
@@ -12,8 +12,8 @@ inputlen = datalen-outputlen
 X = dataset[dataset.columns[0:inputlen]].values
 
 result = []
-for idx, i in enumerate(range(10,25)):
-    print("round: " + str(idx))
+for idx, i in enumerate(range(1,25)):
+    print("...round: " + str(idx))
     # 's1'[1],'s2','s3','s4','s5','w1','w2','w3','w4'
     #,'k1'[10],'k2','k3','k4','k5','k6','k7','k8','k9','k10','k11','k12','k13','k14','k15']
     #select output to predict. Ex. 10 = k1 , 24 = k15
@@ -39,7 +39,7 @@ for idx, i in enumerate(range(10,25)):
     #params['objective'] = 'binary'
     params['metric'] = 'l2_root'
     #params['metric'] = 'binary_logloss'
-    params['sub_feature'] = 0.5
+    params['sub_feature'] = 0.8
     params['num_leaves'] = 30
     params['min_data'] = 50
     params['max_depth'] = 10
@@ -62,21 +62,42 @@ for idx, i in enumerate(range(10,25)):
     print("RMSE_"+ str(i) + ":" +str(rmse))
     result.append(rmse)
 
+print(result)
+avg_result = numpy.mean(result)
+print("AVG of K-RMSE: "+ str(avg_result))
+import numpy
+
+
+N = len(result)
+x = range(1,N+1)
+
+ax = plt.subplot(111)
+ax.set_title('RMSE of K category')
+ax.set_xlabel('K')
+ax.set_xticks(x)
+ax.set_ylabel('RMSE')
+ax.bar(x, result)
+ax.axhline(y=avg_result, color='r', linestyle='-')
+
 #RMSE gencsv_ep100_vec10_Kpre :0.12361350112844519
 #RMSE gencsv_ep100_vec100_Kpre:0.12316789696891321
 
 #======Compare result
 import matplotlib.pyplot as plt
 plt.scatter(y_test, y_pred)
-plt.title('Actual vs Predicted');plt.xlabel('Actual');plt.ylabel('Predicted')
+plt.title('Actual vs Predicted (K15)');plt.xlabel('Actual');plt.ylabel('Predicted')
 plt.ylim(0, 1)
 
 #======Compare result distribution
-n_bins=100
+n_bins=20
 fig, axs = plt.subplots(1, 2, sharey=True, sharex=False, tight_layout=True)
 # We can set the number of bins with the `bins` kwarg
 axs[0].hist(y_test, bins=n_bins)
+axs[0].set_ylabel('Frequency')
+axs[0].set_title('K15-Actual')
 axs[1].hist(y_pred, bins=n_bins)
+axs[1].set_xlabel('Probability')
+axs[1].set_title('K15-Predicted')
 
 #plt.subplot(121)
 #plt.hist(y_test, bins=n_bins)
