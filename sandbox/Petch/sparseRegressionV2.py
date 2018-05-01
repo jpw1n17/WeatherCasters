c5b4d9fff9a66e938dce9c75cf245abc2b8badd8
Petch <32884740+howtolovexz@users.noreply.github.com>
2018-05-02 00:45:00 +0100

    Add files via upload

--- sandbox/Petch/sparseRegressionV2.py
+++ sandbox/Petch/sparseRegressionV2.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 import pandas as pd
 import os
 
@@ -33,38 +35,58 @@ kf.get_n_splits(X)
 x = X.values
 y = Y.values
 
-for train_index, test_index in kf.split(x):
-    x_train, x_test = x[train_index], x[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-
-
-# #############################################################################
-# Compute paths
-
-n_alphas = 100 
-alphas = np.logspace(-10, -2, n_alphas)
-
-coefs = []
+n_alphas = 4 
+alphas = np.logspace(-12, 2, n_alphas)
+rms_errs_all = []
+alphas_all = []
 for a in alphas:
-    ridge = linear_model.Ridge(alpha=a, fit_intercept=False)
-    ridge.fit(x, y)
-    coefs.append(ridge.coef_)
-
-coefs2 = []
-for i in range(0, n_alphas):
-    coef_temp = coefs[i][0]
-    coefs2.append(coef_temp)
+    rms_errs = []
+    for train_index, test_index in kf.split(x):
+        x_train, x_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        
+        ridge = linear_model.Ridge(alpha=a, fit_intercept=False)
+        ridge.fit(x_train, y_train)
+        ridge.coef_
+        
+        y_predicted = ridge.predict(x_test)
+        rms = sqrt(mean_squared_error(y_test, y_predicted))
+        rms_errs.append(rms)
+    rms_errs_all.append(rms_errs)
+    alphas_all.append(a)
     
+colors = ['pink', 'lightblue', 'lightgreen', 'yellow']
+box = plt.boxplot(rms_errs_all, labels = alphas_all)
 
-# #############################################################################
-# Display results
-    
-ax = plt.gca()
-ax.plot(alphas, coefs2)
-ax.set_xscale('log')
-ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
-plt.xlabel('alpha')
-plt.ylabel('weights')
-plt.title('Ridge coefficients as a function of the regularization')
-plt.axis('tight')
-plt.show()
+# =============================================================================
+# # #############################################################################
+# # Compute paths
+# 
+# n_alphas = 100 
+# alphas = np.logspace(-10, -2, n_alphas)
+# 
+# coefs = []
+# for a in alphas:
+#     ridge = linear_model.Ridge(alpha=a, fit_intercept=False)
+#     ridge.fit(x, y)
+#     coefs.append(ridge.coef_)
+# 
+# coefs2 = []
+# for i in range(0, n_alphas):
+#     coef_temp = coefs[i][0]
+#     coefs2.append(coef_temp)
+#     
+# 
+# # #############################################################################
+# # Display results
+#     
+# ax = plt.gca()
+# ax.plot(alphas, coefs2)
+# ax.set_xscale('log')
+# ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
+# plt.xlabel('alpha')
+# plt.ylabel('weights')
+# plt.title('Ridge coefficients as a function of the regularization')
+# plt.axis('tight')
+# plt.show()
+# =============================================================================
