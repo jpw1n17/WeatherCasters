cf47693a6c525e24db72cf5aec03f0202152eb6e
cp2n17soton <32885012+cp2n17soton@users.noreply.github.com>
2018-05-04 22:45:13 +0100

    LGBM
    change prediction model

--- sandbox/champ/visualise/reflect_data.py
+++ sandbox/champ/visualise/reflect_data.py
@@ -43,6 +43,8 @@ from sklearn.metrics import euclidean_distances
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
 
+import lightgbm as lgb
+
 # =============================================================================
 # Redirect the directory
 # =============================================================================
@@ -96,6 +98,38 @@ def create_model(x_train, y_train,gamma):
     ridge.coef_
     return ridge
 
+
+def create_lgbm_model(x_train, y_train):
+    
+    x_train = x_train.values
+    y_train = y_train.values
+    
+    #======Setup parameters
+    d_train = lgb.Dataset(x_train, y_train.flatten())
+    #d_train = lgb.Dataset(x_train, y_train.flatten(),categorical_feature=list(range(10)))
+
+    params = {}
+    params['learning_rate'] = 0.05
+    params['boosting_type'] = 'gbdt'
+    params['objective'] = 'regression_l2'
+    params['metric'] = 'l2_root'
+    params['sub_feature'] = 0.5
+    params['num_leaves'] = 25
+    params['min_data'] = 50
+    params['max_depth'] = 10
+    params['is_unbalance'] = True
+    params['num_iterations'] = 1000
+ 
+    print('start train model : '+str(dt.datetime.now()) )
+    
+    #======Training model
+    clf = lgb.train(params, d_train)
+    
+    print('finish train model : '+str(dt.datetime.now()) )
+    
+    return clf
+    
+    
 # =============================================================================
 # Main Program
 # =============================================================================
@@ -119,13 +153,35 @@ tr_nrows = round(all_nrows*tr_percent) - 1
 
 xtr,ytr,xts,yts = split_train_test(x,y,tr_nrows)
 
-gamma = 0.2
+## =============================================================================
 
-# create and train the Sparse Regression model
-srg = create_model(xtr,ytr,gamma)
-                      
-yhts = srg.predict(xts)
-yhts = pd.DataFrame(yhts)
+#gamma = 0.2
+#
+## create and train the Sparse Regression model
+#srg = create_model(xtr,ytr,gamma)
+#    
+##======Prediction                  
+#yhts = srg.predict(xts)
+
+## =============================================================================
+# transform the result to DataFrame
+#yhts = pd.DataFrame(yhts)
+#
+#yhts_sel = yhts.loc[0:n_pt,23]
+
+## =============================================================================
+
+# selected output column to train the model
+ytr_sel = ytr.loc[:,123]
+
+# create and train the LightGBM model
+clf = create_lgbm_model(xtr,ytr_sel)
+
+#======Prediction
+yhts_sel = clf.predict(xts)
+
+# transform the result to DataFrame
+yhts_sel = pd.DataFrame(yhts_sel)
 
 ## =============================================================================
 ## Plot graph
@@ -143,15 +199,15 @@ plt.rc('font', **font)
 n_point = all_nrows - tr_nrows 
 n_pt = n_point - 1
 
-t = np.arange(0, 1, 0.1)
+ttt = np.arange(0, 1.5, 0.1)
 
 ## =============================================================================
 ## Plot a simple graph
 ## =============================================================================
 
 #plt.figure(1)
-#plt.scatter(yts.loc[tr_nrows:tr_nrows+n_pt,123], yhts.loc[0:n_pt,23])
-#plt.plot(t, t, color='green')
+#plt.scatter(yts.loc[tr_nrows:tr_nrows+n_pt,123], yhts_sel)
+#plt.plot(ttt, ttt, color='green')
 #plt.show()
 
 # =============================================================================
@@ -162,9 +218,9 @@ colors = np.random.rand(n_point)
 
 plt.figure(2)
 fig, ax = plt.subplots(figsize=(10, 6))
-plt.scatter(yts.loc[tr_nrows:tr_nrows+n_pt,123], yhts.loc[0:n_pt,23]
+plt.scatter(yts.loc[tr_nrows:tr_nrows+n_pt,123], yhts_sel
             , c=colors, alpha=0.5)
-plt.plot(t, t, color='green')
+plt.plot(ttt, ttt, color='green')
 
 # add a grid which has grey color
 ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
@@ -207,22 +263,9 @@ col_opt_val = ["#E74C3C","#EB984E","#F4D03F","#2ECC71",
 opt_rng = ["0 - 0.1",">0.1 - 0.3",">0.3 - 0.5",">0.5 - 0.7"
                ,">0.7 - 0.9",">0.9 - 1",">1", "0<"]
 
-# =============================================================================
-# Setting the color of each point
-# =============================================================================
-# color by output value
-indx_lst = []
-colors = []   # list of color
-for i in range(n_point):
-    ### actual output value
-    val = yts.loc[tr_nrows+i,123]
+
+def get_color(val,thrh_val):
     
-    ### predicted output value
-    #val = yhts.loc[i,23]
-    
-    #cl_val =  val
-    
-    thrh_val = [0.1,0.3,0.5,0.7,0.9]
     if val >= 0 and val < float(thrh_val[0]):
         cl_val = "red"
         grp_indx = 0
@@ -253,22 +296,55 @@ for i in range(n_point):
         
     else:
         cl_val = "black"
-        output_range = "0<"
         grp_indx = 7
-        
-            
+    
+    return cl_val,grp_indx
+
+# =============================================================================
+# Setting the color of each point
+# =============================================================================
+# color by actual output value
+indx_lst = []
+colors = []   # list of color
+
+# color by predicted output value
+indx_lst_pd = []
+colors_pd = []
+
+thrh_val = [0.1,0.3,0.5,0.7,0.9]
+
+for i in range(n_point):
+    
+    ### actual output value
+    val_at = yts.loc[tr_nrows+i,123]
+    
+    ### predicted output value
+    val_pd = float(yhts_sel.loc[i,:])
+    
+    #cl_val =  val_at  # simple style
+    
+    ### Actual Output Value
+    cl_val,grp_indx = get_color(val_at,thrh_val)                    
     colors.append(cl_val)
     indx_lst.append(grp_indx)
+    
+    ### Predicted Output Value
+    cl_val_pd,grp_indx_pd = get_color(val_pd,thrh_val)                    
+    colors_pd.append(cl_val_pd)
+    indx_lst_pd.append(grp_indx_pd)    
+    
 colors = np.asarray(colors)
+colors_pd = np.asarray(colors_pd)
+
 # =============================================================================
 #  Plot the graph 
 # =============================================================================
 
 plt.figure(3)
 fig, ax = plt.subplots(figsize=(10, 6))
-plt.scatter(yts.loc[tr_nrows:tr_nrows+n_pt,123], yhts.loc[0:n_pt,23]
+plt.scatter(yts.loc[tr_nrows:tr_nrows+n_pt,123], yhts_sel
             , c=colors, alpha=0.5)
-plt.plot(t, t, color='green')
+plt.plot(ttt, ttt, color='green')
 
 
 # add a grid which has grey color
@@ -286,7 +362,7 @@ ax.set_ylabel('Predicted value')
 plt.show()
 
 # =============================================================================
-# 
+# PCA and MDS
 # =============================================================================
 
 vecdata = xts
@@ -299,10 +375,18 @@ similarities = 1 - cosine_similarity(docvec_data)
 # Multidimensional Scaling for Doc2Vec data
 seed = np.random.RandomState(seed=3)
 
-mds = manifold.MDS(n_components=2, max_iter=1, eps=1e-9, random_state=seed,
+mds = manifold.MDS(n_components=2, max_iter=10, eps=1e-9, random_state=seed,
                    dissimilarity="precomputed", n_jobs=1)
 
+# Map the vectors into 2D using MDS and the similarities of vectors 
+# =============================================================================
+print('start MDS : '+str(dt.datetime.now()) )
+# =============================================================================
 pos = mds.fit(similarities).embedding_
+# =============================================================================
+print('finish MDS : '+str(dt.datetime.now()) )
+# =============================================================================
+
 
 # Rotate the data
 clf = PCA(n_components=2)
@@ -314,31 +398,30 @@ pca_docvecs = clf.fit_transform(docvec_data)
 dv_df = pd.DataFrame(pca_docvecs)
 pos_df = pd.DataFrame(pos)
 
+# =============================================================================
+# plot graph using the names of colour
+# =============================================================================
+#pltvec_df = dv_df
+#
+#plt.figure(4)
+#fig, ax = plt.subplots(figsize=(10, 6))
+#plt.scatter( pltvec_df.loc[:,0] ,pltvec_df.loc[:,1]
+#            , c=colors, alpha=0.5)
+#
+## add a grid which has grey color
+#ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+#               alpha=0.5)
+#ax.xaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+#               alpha=0.5)
+#
+#ax.set_axisbelow(True)
+#
+#plt.show()
+
+# =============================================================================
+# grouping data by the colour of actual output 
+# =============================================================================
 pltvec_df = dv_df
-
-
-# =============================================================================
-# plot graph
-# =============================================================================
-
-plt.figure(4)
-fig, ax = plt.subplots(figsize=(10, 6))
-plt.scatter( pltvec_df.loc[:,0] ,pltvec_df.loc[:,1]
-            , c=colors, alpha=0.5)
-
-# add a grid which has grey color
-ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-ax.xaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-
-ax.set_axisbelow(True)
-
-plt.show()
-
-# =============================================================================
-# grouping data by the colour
-# =============================================================================
 
 # Create data frame that has the result 
 df = pd.DataFrame(dict(x=pltvec_df.loc[:,0], y=pltvec_df.loc[:,1], label=indx_lst)) 
@@ -373,7 +456,7 @@ plt.show()
 
 
 # =============================================================================
-# grouping MDS vector by the colour
+# grouping MDS vector by the colour of actual output 
 # =============================================================================
 pltvec_df = pos_df
 
@@ -407,6 +490,87 @@ ax.xaxis.grid(True, linestyle='-', which='major', color='lightgrey',
 ax.set_axisbelow(True)
 ax.set_title('2D Visualisation Coloured by Actual Output Value')
 plt.show()
+
+# =============================================================================
+# grouping data by the colour of predicted output 
+# =============================================================================
+pltvec_df = dv_df
+
+# Create data frame that has the result 
+df = pd.DataFrame(dict(x=pltvec_df.loc[:,0], y=pltvec_df.loc[:,1], label=indx_lst_pd)) 
+# Group by cluster
+groups = df.groupby('label')
+
+# =============================================================================
+# Visualise PCA vectors in 2D scatter plot
+# =============================================================================
+
+plt.figure(7)
+fig, ax = plt.subplots(figsize=(10, 6))
+for indx, group in groups:
+    ax.plot( group.x, group.y
+            , marker='o', linestyle='',ms=4 
+            , color=col_opt_val[indx]
+            , label=opt_rng[indx]
+            )
+    
+# add legend    
+plt.legend(scatterpoints=1, loc='best', shadow=False)
+
+# add a grid which has grey color
+ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+               alpha=0.5)
+ax.xaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+               alpha=0.5)
+
+ax.set_axisbelow(True)
+ax.set_title('2D Visualisation Coloured by Predicted Output Value')
+plt.show()
+
+
+# =============================================================================
+# grouping MDS vector by the colour of predicted output 
+# =============================================================================
+pltvec_df = pos_df
+
+# Create data frame that has the result 
+df = pd.DataFrame(dict(x=pltvec_df.loc[:,0], y=pltvec_df.loc[:,1], label=indx_lst_pd)) 
+# Group by cluster
+groups = df.groupby('label')
+
+# =============================================================================
+# Visualise vectors using MDS in 2D scatter plot
+# =============================================================================
+
+plt.figure(8)
+fig, ax = plt.subplots(figsize=(10, 6))
+for indx, group in groups:
+    ax.plot( group.x, group.y
+            , marker='o', linestyle='',ms=4 
+            , color=col_opt_val[indx]
+            , label=opt_rng[indx]
+            )
+    
+# add legend    
+plt.legend(scatterpoints=1, loc='best', shadow=False)
+
+# add a grid which has grey color
+ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+               alpha=0.5)
+ax.xaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+               alpha=0.5)
+
+ax.set_axisbelow(True)
+ax.set_title('2D Visualisation Coloured by Predicted Output Value')
+plt.show()
+
+
+# =============================================================================
+# 
+# =============================================================================
+
+
+
 
 
 # =============================================================================
